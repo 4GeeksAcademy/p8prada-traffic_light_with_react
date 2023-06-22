c37f8def0b6869d63d67957ed75d7f8f416176c7
p8prada <131833040+p8prada@users.noreply.github.com>
2023-06-22 13:29:19 -0400

    testing vscode for windows

--- src/js/component/ChangeLight.jsx
+++ src/js/component/ChangeLight.jsx
@@ -4,11 +4,7 @@ const Home = () => {
 	const [selectedLight, setSelectedLight] = useState("red");
 	return (
 		<div className="stick">
-			<div className="trafficLightBox">
-				<div onClick={() =>setSelectedLight("red")} className={"light red" + ((selectedLight === "red") ? " glowRed" : "")}></div>
-				<div onClick={() =>setSelectedLight("yellow")} className={"light yellow" + ((selectedLight === "yellow") ? " glowYellow" : "")}></div>
-				<div onClick={() =>setSelectedLight("green")} className={"light green" + ((selectedLight === "green") ? " glowGreen" : "")}></div>
-			</div>
+
 		</div>
 	);
 };
